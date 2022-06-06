8da23c860983457ec24f835d8ae51f97482a8f52
AdamFASHOO <AdamChapman1994@gmail.com>
2022-06-06 17:20:55 -0400

    day 1 commit

---
+++ UpmeetEventSystem/Controllers/EventController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UpmeetEventSystem.Models;
+
+namespace UpmeetEventSystem.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        EventDBContext db = new EventDBContext();
+        
+        [HttpGet("GetEvents")]
+        public List<Event> GetEvents()
+        {
+            return db.Events.ToList();
+        }
+
+        [HttpGet("SearchByName/{name}")]
+        public List<Event> SearchByName(string name)
+        {
+            List<Event> events = db.Events.Where(m => m.EventName.Contains(name)).ToList();
+            return events;
+        }
+
+        [HttpGet("SearchById/{id}")]
+        public Event GetEventById(int id)
+        {
+            return db.Events.Where(x => x.EventId == id).First();
+        }
+
+        [HttpPost("CreateEvent")]
+        public string CreateEvent(Event e)
+        {
+            db.Events.Add(e);
+            db.SaveChanges();
+            return e.EventName + " has been successfully added to the database";
+        }
+
+        [HttpDelete("DeleteEvent/{id}")]
+        public string DeleteEvent(int id)
+        {
+            Event e = db.Events.Find(id);
+            db.Events.Remove(e);
+            db.SaveChanges();
+            return $"The Event at id {id} has been deleted successfully";
+        }
+
+        [HttpPut("UpdateEvent/{id}")]
+        public string UpdateEvent(int id, Event updatedInfo)
+        {
+            Event e = db.Events.Find(id);
+
+            e.EventName = updatedInfo.EventName;
+            e.EventDate = updatedInfo.EventDate;
+            e.EventLocation = updatedInfo.EventLocation;
+            e.IsFavorite = updatedInfo.IsFavorite;
+
+            db.Events.Update(e);
+            db.SaveChanges();
+
+            return $"The Event {e.EventName} has been updated at id {e.EventId}";
+        }
+
+        [HttpPut("FavoriteEvent/{id}")]
+        public string CompleteEvent(int id)
+        {
+            Event e = db.Events.Find(id);
+
+            e.IsFavorite = true;
+            db.Events.Update(e);
+            db.SaveChanges();
+
+            return $"{e.EventName} has been favorited";
+        }
+    }
+}
---
+++ UpmeetEventSystem/Models/EventDBContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace UpmeetEventSystem.Models
+{
+    public partial class EventDBContext : DbContext
+    {
+        public EventDBContext()
+        {
+        }
+
+        public EventDBContext(DbContextOptions<EventDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Event> Events { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=EventDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Event>(entity =>
+            {
+                entity.Property(e => e.EventId).HasColumnName("EventID");
+
+                entity.Property(e => e.EventDate).HasColumnType("date");
+
+                entity.Property(e => e.EventLocation).HasMaxLength(40);
+
+                entity.Property(e => e.EventName).HasMaxLength(40);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
